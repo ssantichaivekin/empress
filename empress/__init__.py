25ec3066f321fde1e9a23a837d1d9c29c1bc6e83
ssantichaivekin <jsantichaivekin@g.hmc.edu>
2020-06-03 17:26:56 -0700

    Rebase from master

--- empress/__init__.py
+++ empress/__init__.py
@@ -2,9 +2,14 @@
 Wraps empress functionalities
 """
 import matplotlib.pyplot as plt
-from typing import List, Iterable
+from typing import List
 from abc import ABC, abstractmethod
+
 from empress.xscape.CostVector import CostVector
+from empress import newickFormatReader
+from empress.xscape.reconcile import reconcile as xscape_reconcile
+from empress.xscape.plotcostsAnalytic import plot_costs_to_axis as xscape_plot_costs_to_axis
+
 
 class Drawable(ABC):
     """
@@ -22,7 +27,9 @@ class Drawable(ABC):
         """
         Draw self as matplotlib Figure.
         """
-        pass
+        figure, ax = plt.subplots(1, 1)
+        self.draw_on(ax)
+        return figure
 
     def draw_to_file(self, fname):
         """
@@ -31,11 +38,13 @@ class Drawable(ABC):
         figure = self.draw()
         figure.savefig(fname)
 
+
 class ReconInputWrapper:
     def __init__(self, parasite_tree: dict, host_tree: dict, tip_mapping: dict):
         self._parasite_tree = parasite_tree
         self._host_tree = host_tree
         self._tip_mapping = tip_mapping
+
 
 class ReconciliationWrapper(Drawable):
     # TODO: Replace dict with Reconciliation type
@@ -45,6 +54,7 @@ class ReconciliationWrapper(Drawable):
 
     def draw_on(self, axes: plt.Axes):
         pass
+
 
 class ReconGraphWrapper(Drawable):
     # TODO: Replace dict with ReconGraph type
@@ -67,29 +77,46 @@ class ReconGraphWrapper(Drawable):
         """
         pass
 
+
 class CostRegionWrapper(Drawable):
-    def __init__(self, info):
+    def __init__(self, cost_vectors, switch_min, switch_max, loss_min, loss_max):
         """
         Will look into this more.
         """
+        self._cost_vectors = cost_vectors
+        self._switch_min = switch_min
+        self._switch_max = switch_max
+        self._loss_min = loss_min
+        self._loss_max = loss_max
         pass
 
-    def draw_on(self, axes: plt.Axes):
-        pass
+    def draw_on(self, axes: plt.Axes, log=False):
+        xscape_plot_costs_to_axis(axes, self._cost_vectors, self._switch_min, self._switch_max,
+                                  self._loss_min, self._loss_max, log=False)
+
 
 def read_input(fname: str) -> ReconInputWrapper:
     """
     Read parasite tree, host tree, and tip mapping from fname.
     Returns ReconInputWrapper which wraps the three info.
     """
-    pass
+    host_tree, parasite_tree, tip_mapping = newickFormatReader.getInput(fname)
+    recon_input = ReconInputWrapper(parasite_tree, host_tree, tip_mapping)
+    return recon_input
 
-def compute_cost_region(recon_input: ReconInputWrapper) -> CostRegionWrapper:
+
+def compute_cost_region(recon_input: ReconInputWrapper, switch_low: float, switch_high: float,
+                         lost_low: float, lost_high: float) -> CostPolygonWrapper:
     """
     Compute the cost polygon of recon_input. The cost polygon can be used
     to create a figure that separate costs into different regions.
     """
-    pass
+    parasite_tree = recon_input._parasite_tree
+    host_tree = recon_input._host_tree
+    tip_mapping = recon_input._tip_mapping
+    cost_vectors = xscape_reconcile(parasite_tree, host_tree, tip_mapping, switch_low, switch_high, lost_low, lost_high)
+    return CostPolygonWrapper(cost_vectors, switch_low, switch_high, lost_low, lost_high)
+
 
 def reconcile(recon_input: ReconInputWrapper, dup_cost: int, trans_cost: int, loss_cost: int) -> ReconGraphWrapper:
     """
