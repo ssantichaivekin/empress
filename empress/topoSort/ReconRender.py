9ce86546dd683cdf0cfad721e2d2bd1c91592ce8
TrentonJW <twesley@hmc.edu>
2020-06-12 17:21:02 -0500

    Added Constants to Constants.py and added some function headers

--- empress/topoSort/ReconRender.py
+++ empress/topoSort/ReconRender.py
@@ -12,27 +12,43 @@ def render_tree(host_tree, parasite_tree, reconciliation):
     :param host_tree:  host tree dictionary
     :param parasite_tree:  parasite tree dictionary
     :param reconciliation:  reconciliation dictionary
-    :return: None
     """
    
+    #Sets up canvas
+    window = tk.Tk()
+    window.title(cons.WINDOW_NAME)
+
+    frame = tk.Frame(window)
+    frame.pack()
+
+    canvas = tk.Canvas(frame, width = cons.WINDOW_WIDTH, height = cons.WINDOW_HEIGHT)
+    canvas.pack()
+
     #Create our tree objects
     host_tree_object, parasite_tree_object = build_trees_with_temporal_order(host_tree, parasite_tree, reconciliation)
     
-
-    #Set the values for the logical rows of our tree object
+    #Set the values for the logical rows of our tree objects
     compute_node_logical_rows(host_tree_object, parasite_tree_object, reconciliation)
 
 
+    #Render Host Tree
+    render_host_tree(host_tree_root, canvas)
+
+    #Displays canvas
+    canvas.pack()
+    window.mainloop()
+
+
+
 def compute_node_logical_rows(host_tree_object, parasite_tree_object, reconciliation):
-    '''
+    """
     Sets the logicalRow values of each Node in the host and parasite tree.
     Assumes that each Node has its logical column set already and this function
     uses those values and the reconfiguration to set logicalRow and logicalCol
     :param host_tree_object:  A Tree object representing the host tree
     :param parasite_tree_object:  A Tree object representing the parasite tree
     :param reconciliation:  reconciliation dictionary
-    :return None
-    '''
+    """
 
     compute_host_logical_rows(host_tree_object)
     compute_parasite_logical_rows(parasite_tree_object, reconciliation)
@@ -41,13 +57,12 @@ def compute_node_logical_rows(host_tree_object, parasite_tree_object, reconcilia
 
 
 def compute_host_logical_rows(host_tree_object):
-    '''
+    """
     Sets the logicalRow values of each Node in the host tree.
     Assumes that each host Node has its column set already and this function
     uses those values and structure of the tree to set the logicalRow
     :param host_tree_object:  A Tree object representing the host tree
-    :return None
-    '''
+    """
 
     row_counter = 0
     for node in host_tree_object:
@@ -59,35 +74,42 @@ def compute_host_logical_rows(host_tree_object):
 
 
 def compute_parasite_logical_rows(parasite_tree_object, reconciliation):
-    '''
+    """
     Sets the logicalRow values of each Node in the parasite tree.
     Assumes that each host Node has its column set already and this function
     uses those values and structure of the tree to set the logicalRow
     :param parasite_tree_object:  A Tree object representing the parasite tree
-    :return None
-    '''
-
+    """
     # TODO Get this to work with new Recon class
-
-
 
 
 def compute_host_node_actual_positions(host_tree_object, canvas):
     """
     Sets the xcoord and ycoord for rendering the Node host tree
-    :param host_tree:  A Tree object representing the host tree
-    :parem x_min: Integer; minimum x-coordinate for tree rendering
-    :param x_max: Integer; maximum y-coordinate for tree rendering
-    :param y_min: Integer; minimum y-coordinate for tree rendering
-    :param y_max: Integer; maximum y-coordinate for tree rendering
-    :return: None
+    :param host_tree_object:  A Tree object representing the host tree
+    :param canvas: Drawing Canvas from tkinter
     """
+    #Gives us the boundaries of the drawing space on canvas along with units
+    x_min, y_min, x_max, y_max, x_unit, y_unit = set_parameters(canvas)
 
+    #Gives us the unit space between logical positions
+    x_unit, y_unit = set_units()
+
+def set_boundaries(canvas):
+    pass
+
+def set_parameters(canvas):
+    #x_min, y_min, x_max, y_max = set_boundaries(canvas)
 
 def render_host_tree(host_tree_root, canvas):
-    '''
+    """
     Draws the given host tree using tkinter
     :param host_tree_object: A Tree object representing the host tree
     :param canvas: Drawing canvas from tkinter
-    :return None
-    '''
+    """
+
+    #Populates each node with x and y values for their actual positions
+    compute_host_node_actual_positions(host_tree_object, canvas)
+
+
+    
