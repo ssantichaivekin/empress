153e2736c67d1fa898f175b0386f491e1a4b950b
ssantichaivekin <jsantichaivekin@g.hmc.edu>
2020-06-17 15:23:05 -0700

    Modify tests to use unittest

--- test/test_empress_wrapper.py
+++ test/test_empress_wrapper.py
@@ -1,5 +1,6 @@
 import empress
 import unittest
+import os
 import matplotlib.pyplot as plt
 
 class TestEmpressWrappers(unittest.TestCase):
@@ -13,8 +14,18 @@ class TestEmpressWrappers(unittest.TestCase):
     def test_compute_cost_regions(self):
         recon_input = empress.read_input(self.example_input_path)
         cost_regions = empress.compute_cost_regions(recon_input, 0.5, 10, 0.5, 10)
+        self.assertTrue(isinstance(cost_regions, empress.CostRegionsWrapper))
+
+    def test_draw_cost_regions(self):
+        # TODO: move to visualization test
+        # https://github.com/ssantichaivekin/eMPRess/issues/83
+        recon_input = empress.read_input(self.example_input_path)
+        cost_regions = empress.compute_cost_regions(recon_input, 0.5, 10, 0.5, 10)
         fig = cost_regions.draw()
-        self.assertTrue(isinstance(fig, plt.Axes))
+        filename = "./cost_region_draw_example.png"
+        fig.savefig(filename)
+        os.path.exists(filename)
+        os.remove(filename)
 
     def test_reconcile(self):
         recon_input = empress.read_input(self.example_input_path)
@@ -22,64 +33,85 @@ class TestEmpressWrappers(unittest.TestCase):
         self.assertTrue(isinstance(recongraph, empress.ReconGraphWrapper))
         self.assertTrue(isinstance(recongraph.n_recon, int))
         # the testing recongraph should be designed to have multiple MPRs for DTL = 111
-        self.assertGreater(recongraph, 1)
+        self.assertGreater(recongraph.n_recon, 1)
 
-    def test_draw_recongraph(self):
+    def test_recongraph_draw(self):
         recon_input = empress.read_input(self.example_input_path)
         recongraph = empress.reconcile(recon_input, 1, 1, 1)
         fig = recongraph.draw()
-        # Uncomment for examples:
-        # recongraph.draw_graph_to_file("./recongraph_example.pdf")
-        # recongraph.draw_to_file("./recongraph_hist_example.pdf")
-        self.assertTrue(isinstance(fig, plt.Axes))
+        self.assertTrue(isinstance(fig, plt.Figure))
+
+    def test_recongraph_draw_graph(self):
+        # Draw reconciliation graph to file
+        recon_input = empress.read_input(self.example_input_path)
+        recongraph = empress.reconcile(recon_input, 1, 1, 1)
+        fig = recongraph.draw()
+        self.assertTrue(isinstance(fig, plt.Figure))
 
     def test_median(self):
+        recon_input = empress.read_input(self.example_input_path)
+        recongraph = empress.reconcile(recon_input, 1, 1, 1)
+        median_reconciliation = recongraph.median()
+        self.assertTrue(isinstance(median_reconciliation, empress.ReconciliationWrapper))
 
+    def test_reconciliation_draw(self):
+        recon_input = empress.read_input(self.example_input_path)
+        recongraph = empress.reconcile(recon_input, 1, 1, 1)
+        median_reconciliation = recongraph.median()
+        fig = median_reconciliation.draw()
+        self.assertTrue(isinstance(fig, plt.Figure))
 
-# Find median
-median_reconciliation = recongraph.median()
+    def test_clusters(self):
+        recon_input = empress.read_input(self.example_input_path)
+        recongraph = empress.reconcile(recon_input, 1, 1, 1)
+        clusters = recongraph.cluster(3)
+        self.assertTrue(isinstance(clusters, list))
+        total_recon = 0
+        for cluster in clusters:
+            self.assertTrue(isinstance(cluster, empress.ReconGraphWrapper))
+            total_recon += cluster.n_recon
+        # n_recon ~ number of reconciliation graphs
+        self.assertEqual(total_recon, recongraph.n_recon)
 
-# Get Reconciliation figure object for visualization
-# Note that you can use this for tkinter
-fig = median_reconciliation.draw()
-# Save Reconciliation visualization to file
-median_reconciliation.draw_to_file("./recon_draw_example.png")
+    def test_cluster_pdv_histogram_draw(self):
+        # TODO: move to visualization test
+        # https://github.com/ssantichaivekin/eMPRess/issues/83
+        recon_input = empress.read_input(self.example_input_path)
+        recongraph = empress.reconcile(recon_input, 1, 1, 1)
+        clusters = recongraph.cluster(3)
+        fig, axs = plt.subplots(3, 3)
+        # first row -- one cluster
+        recongraph.draw_on(axs[0, 0])
+        # second row -- two clusters
+        clusters_2 = recongraph.cluster(2)
+        clusters_2[0].draw_on(axs[1, 0])
+        clusters_2[1].draw_on(axs[1, 1])
+        # third row -- three clusters
+        clusters_3 = recongraph.cluster(3)
+        clusters_3[0].draw_on(axs[2, 0])
+        clusters_3[1].draw_on(axs[2, 1])
+        clusters_3[2].draw_on(axs[2, 2])
 
-# Separate one ReconGraph to three ReconGraph
-# clusters is a list of ReconGraph
-clusters = recongraph.cluster(3)
+        filename = "./temp_multi_pdv_histogram_example.png"
+        fig.savefig(filename)
+        os.path.exists(filename)
+        os.remove(filename)
 
-# number of reconciliation graph
-recongraph.n_recon
+    def test_cluster_reconcile_draw(self):
+        # TODO: move to visualization test
+        # https://github.com/ssantichaivekin/eMPRess/issues/83
+        recon_input = empress.read_input(self.example_input_path)
+        recongraph = empress.reconcile(recon_input, 1, 1, 1)
+        clusters = recongraph.cluster(3)
+        fig, axs = plt.subplots(1, len(clusters))
+        for i in range(len(clusters)):
+            median = clusters[i].median()
+            median.draw_on(axs[i])
 
-# Draw three histograms on one figure
-# fig: plt.Figure
-fig, axs = plt.subplots(1, len(clusters))
-for i in range(len(clusters)):
-    clusters[i].draw_on(axs[i])
-# Note that you could also display this figure in tkinter
-fig.savefig("./figure_of_three_his_example.png")
+        filename = "./temp_three_medians_example.png"
+        fig.savefig(filename)
+        os.path.exists(filename)
+        os.remove(filename)
 
-# Draw three Reconciliations on one figure
-fig, axs = plt.subplots(1, len(clusters))
-for i in range(len(clusters)):
-    median = clusters[i].median()
-    median.draw_on(axs[i])
-# Note that you could also display this figure in tkinter
-fig.savefig("./figure_of_three_medians_example.png")
-
-# Read this example and see whether you get what it does
-fig, axs = plt.subplots(3, 3)
-# first row -- one cluster
-recongraph.draw_on(axs[0, 0])
-# second row -- two clusters
-clusters_2 = recongraph.cluster(2)
-clusters_2[0].draw_on(axs[1, 0])
-clusters_2[1].draw_on(axs[1, 1])
-# third row -- three clusters
-clusters_3 = recongraph.cluster(3)
-clusters_3[0].draw_on(axs[2, 0])
-clusters_3[1].draw_on(axs[2, 1])
-clusters_3[2].draw_on(axs[2, 2])
-
-fig.savefig("./multi_pdv_histogram_example.pdf")
+if __name__ == '__main__':
+    unittest.main()
