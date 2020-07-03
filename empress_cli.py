939e908bf7818e5a57d08233942c0e986c269930
ssantichaivekin <jsantichaivekin@g.hmc.edu>
2020-07-02 17:19:32 -0700

    Fix typo

--- empress_cli.py
+++ empress_cli.py
@@ -7,7 +7,7 @@
 import argparse
 from pathlib import Path
 
-from empress
+import empress
 from empress.reconcile import recongraph_tools
 from empress.cluster import cluster_main
 from empress.histogram import histogram_main
