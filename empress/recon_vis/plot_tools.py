1b6b1f6cd843b3113b5480f3aedf87c8142c7853
TrentonJW <twesley@hmc.edu>
2020-07-15 20:34:07 -0500

    Added type hints to plot_tools.py

--- empress/recon_vis/plot_tools.py
+++ empress/recon_vis/plot_tools.py
@@ -38,7 +38,7 @@ class Position(NamedTuple):
 
 class FigureWrapper:
     """ Class definining plotting methods """
-    def __init__(self, title, axes: Union[plt.Axes, None] = None):
+    def __init__(self, title: str, axes: Union[plt.Axes, None] = None):
         """
         If axes is specified, draw on axes instead.
         """
@@ -53,14 +53,14 @@ class FigureWrapper:
         self.axis.axis("off")
         self.axis.set_title(title)
 
-    def set_legend(self, legend_elements, loc='best', fontsize=FONTSIZE, title=None):
+    def set_legend(self, legend_elements: list, loc: str = 'best', fontsize: int = FONTSIZE, title: str = None):
         """
         create legend
         """
 
         self.axis.legend(handles=legend_elements, loc=loc, fontsize=fontsize, title=title)
         
-    def line(self, point_1, point_2, col=BLACK, linestyle='-'):
+    def line(self, point_1: Position, point_2: Position, col: tuple = BLACK, linestyle: str = '-'):
         """
         Draw line from point p1 to p2
         """
@@ -68,18 +68,18 @@ class FigureWrapper:
         x_2, y_2 = point_2
         self.axis.plot([x_1, x_2], [y_1, y_2], color=col, linewidth=LINEWIDTH, linestyle=linestyle, zorder=LINE_Z_ORDER)
 
-    def dot(self, point, marker='o', col=BLACK):
+    def dot(self, point: Position, marker: str = 'o', col: tuple = BLACK):
         """
         Plot dot at point p
         """
         x, y = point
         self.axis.plot(x, y, marker, color=col, zorder=DOT_Z_ORDER)
     
-    def text(self, point, string, col=RED, h_a='right'):
+    def text(self, point: tuple, string: str, col: tuple = RED, h_a: str = 'right'):
         x, y = point
         self.axis.text(x, y, string, color=col, fontsize=FONTSIZE, horizontalalignment=h_a, verticalalignment='top')
 
-    def text_v2(self, point, text, col=BLACK, size=SIZE, vertical_alignment=DEFAULT_ALIGNMENT, border_col=None):
+    def text_v2(self, point: tuple, text: str, col: tuple = BLACK, size: float = SIZE, vertical_alignment: str = DEFAULT_ALIGNMENT, border_col: tuple = None):
         """
         Plot text at s at point p
         """
@@ -87,7 +87,7 @@ class FigureWrapper:
             if vertical_alignment == 'center':
                 point = (point[0], point[1] - size * CENTER_CONSTANT)
 
-            tp = TextPath(point, text, size= size)
+            tp = TextPath(point, text, size=size)
             path_patch = PathPatch(tp, color=col, linewidth = TEXTWIDTH, zorder=TEXT_Z_ORDER)
             if border_col:
                 path_patch.set_path_effects([PathEffects.withStroke(linewidth=BORDER_WIDTH, foreground=border_col)])
@@ -100,13 +100,13 @@ class FigureWrapper:
         plt.figure(self.fig.number)
         plt.show()
 
-    def save(self, filename):
+    def save(self, filename: str):
         """
         Save figure to file
         """
         self.fig.savefig(filename)
     
-    def half_arrow(self, point_1, point_2, col=BLACK):
+    def half_arrow(self, point_1: Position, point_2: Position, col: tuple = BLACK):
         """
         Draw arrow from point p1 to p2
         """
@@ -114,14 +114,14 @@ class FigureWrapper:
         x_2, y_2 = point_2
         self.axis.arrow(x_1, y_1, 0, abs(y_2-y_1)/2, head_width=0.15, head_length=0.15, color=col, linewidth=LINEWIDTH/2, shape='full', length_includes_head=True, zorder=LINE_Z_ORDER)
 
-    def up_triangle(self, point, col=BLACK, markersize=TRANSFERSIZE):
+    def up_triangle(self, point: Position, col: tuple = BLACK, markersize: int = TRANSFERSIZE):
         """
         Draw an upwards triangle on point
         """
         x, y = point
         self.axis.plot(x, y, '^', color=col, zorder=LINE_Z_ORDER, markersize=TRANSFERSIZE)
 
-    def down_triangle(self, point, col=BLACK, markersize=TRANSFERSIZE):
+    def down_triangle(self, point: Position, col: tuple = BLACK, markersize: int = TRANSFERSIZE):
         """
         Draw an downwards triangle on point
         """
