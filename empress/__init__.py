ssantichaivekin/empress

commit 8e99a1b070ca386f0fcb4eec848cfef768d357c0

GUI: Add font size to tanglegram and reconciliation window (#240)
- Add a font size input text box in the tanglegram window that allows changing the font size of node names.
- Add a redraw button for user to click after changing the font size.
- Add the same font size text box and redraw button to the reconciliation window.

empress/__init__.py
@@ -53,27 +53,35 @@ class Drawable(ABC):
     """
 
     @abstractmethod
-    def draw_on(self, axes: plt.Axes):
+    def draw_on(self, axes: plt.Axes, **kwargs):
         """
         Draw self on matplotlib Axes
         """
         pass
 
-    def draw(self) -> plt.Figure:
+    def draw(self, **kwargs) -> plt.Figure:
         """
         Draw self as matplotlib Figure.
         """
         figure, ax = plt.subplots(1, 1)
-        self.draw_on(ax)
+        self.draw_on(ax, **kwargs)
         return figure
 
 
 class ReconciliationWrapper(Drawable):
     # TODO: Replace dict with Reconciliation type
     # https://github.com/ssantichaivekin/eMPRess/issues/30
-    def __init__(self, reconciliation: dict, root: tuple, recon_input: _ReconInput, dup_cost, trans_cost, loss_cost,
-            total_cost: float, event_frequencies: Dict[tuple, float],
-            node_frequencies: Dict[tuple, float] = None):
+    def __init__(self,
+                 reconciliation: dict,
+                 root: tuple,
+                 recon_input: _ReconInput,
+                 dup_cost,
+                 trans_cost,
+                 loss_cost,
+                 total_cost: float,
+                 event_frequencies: Dict[tuple, float],
+                 node_frequencies: Dict[tuple, float] = None
+                 ):
         self.recon_input = recon_input
         self.dup_cost = dup_cost
         self.trans_cost = trans_cost
@@ -84,15 +92,22 @@ class ReconciliationWrapper(Drawable):
         self.event_frequencies = event_frequencies
         self.node_frequencies = node_frequencies
 
-    def draw(self, show_internal_labels: bool = False, show_freq: bool = True):
-        figure, ax = plt.subplots(1, 1)
-        self.draw_on(ax, show_internal_labels=show_internal_labels, show_freq=show_freq)
-        return figure
-
-    def draw_on(self, axes: plt.Axes, show_internal_labels: bool = False, show_freq: bool = True):
-        recon_viewer.render(self.recon_input.host_dict, self.recon_input.parasite_dict, self._reconciliation,
-                            self.event_frequencies, show_internal_labels=show_internal_labels, show_freq=show_freq,
-                            axes=axes)
+    def draw_on(self,
+                axes: plt.Axes,
+                show_internal_labels: bool = False,
+                show_freq: bool = True,
+                node_font_size: float = 0.3,
+                ):
+        recon_viewer.render(
+            self.recon_input.host_dict,
+            self.recon_input.parasite_dict,
+            self._reconciliation,
+            self.event_frequencies,
+            show_internal_labels=show_internal_labels,
+            show_freq=show_freq,
+            node_font_size=node_font_size,
+            axes=axes
+        )
 
     def count_events(self) -> Tuple[int, int, int, int]:
         """
@@ -249,11 +264,11 @@ class ReconInputWrapper(_ReconInput, Drawable):
     def __init__(self, *args, **kwargs):
         _ReconInput.__init__(self, *args, **kwargs)
 
-    def draw_on(self, ax: plt.Axes):
+    def draw_on(self, ax: plt.Axes, node_font_size=9):
         """
         This draws the tanglegram.
         """
-        tanglegram.render(self.host_dict, self.parasite_dict, self.tip_mapping, True, ax)
+        tanglegram.render(self.host_dict, self.parasite_dict, self.tip_mapping, True, ax, node_font_size=node_font_size)
 
     def compute_cost_regions(self, transfer_min: float, transfer_max: float,
                              dup_min: float, dup_max: float) -> CostRegionsWrapper:

empress/recon_vis/recon_viewer.py
@@ -12,8 +12,16 @@ import matplotlib.pyplot as plt
 FONT_SIZE_STRETCH = 3.0     # Parameter used in _calculate_font_size
 SIGMOID_SCALE = 0.8         # Parameter used in _sigmoid
 
-def render(host_dict: dict, parasite_dict: dict, recon_dict: dict, event_frequencies: Dict[tuple, float] = None, show_internal_labels: bool = False, 
-           show_freq: bool = False, show_legend: bool = True, axes: Union[plt.Axes, None] = None):
+def render(host_dict: dict,
+           parasite_dict: dict,
+           recon_dict: dict,
+           event_frequencies: Dict[tuple, float] = None,
+           show_internal_labels: bool = False,
+           show_freq: bool = False,
+           show_legend: bool = True,
+           node_font_size: float = 0.3,
+           axes: Union[plt.Axes, None] = None,
+           ):
     """ Renders a reconciliation using matplotlib
     :param host_dict:  Host tree represented in dictionary format
     :param parasite_dict:  Parasite tree represented in dictionary format
@@ -36,9 +44,7 @@ def render(host_dict: dict, parasite_dict: dict, recon_dict: dict, event_frequen
     if show_legend:
         _create_legend(fig, consistency_type)
 
-    # Calculates font sizes
-    num_tip_nodes = len(host_tree.leaf_list()) + len(parasite_tree.leaf_list())
-    font_size = _calculate_font_size(num_tip_nodes)
+    font_size = node_font_size
 
     root = parasite_tree.root_node
     host_lookup = host_tree.name_to_node_dict()
@@ -371,25 +377,6 @@ def _get_frequency_text(frequency: float):
     :return a string that has the frequency as a percentage
     """
     return str(round(frequency * 100))
-
-
-def _calculate_font_size(num_tip_nodes: int):
-    """
-    Calculates the font_size
-    :param num_tip_nodes: Number of tip nodes in a tree
-    :return the font size for the tips and internal nodes of a tree
-    """
-    x = (render_settings.START_SIZE - num_tip_nodes) / render_settings.STEP_SIZE
-    return FONT_SIZE_STRETCH * _sigmoid(x)  # 3.0 is a magic value that can be adjusted
-
-
-def _sigmoid(x: float):
-    """
-    sigmoid Function
-    :param x: A number to be plugged into the function
-    :return a sigmoid value based on the input value, x
-    """
-    return (1 / (1 + math.e**(-SIGMOID_SCALE*x)))  # 0.8 is a magic value that can be adjusted
 
 
 def _render_parasite_branches(fig: plot_tools.FigureWrapper,  node: tree.Node, recon_obj: recon.Reconciliation, host_lookup: dict, parasite_lookup: dict):

empress/recon_vis/tanglegram.py
@@ -12,14 +12,19 @@ VERTICAL_OFFSET = 20
 HORIZONTAL_SPACING = 10
 LEAF_SPACING = 5
 EXTRA_SPACING_FOR_LABEL = 50
-FONTSIZE = 9
 
 # global variables
 _g_host_counter = 0
 _g_parasite_counter = 0
 
-def render(host_dict: dict, parasite_dict: dict, tip_mapping: dict, show_internal_labels: bool, ax: plt.Axes = None) \
-        -> plot_tools.FigureWrapper:
+def render(
+    host_dict: dict,
+    parasite_dict: dict,
+    tip_mapping: dict,
+    show_internal_labels: bool,
+    ax: plt.Axes = None,
+    node_font_size = 9,
+    ) -> plot_tools.FigureWrapper:
     """
     Render tanglegram
     :param host_dict - host tree (dictionary representation)
@@ -38,8 +43,18 @@ def render(host_dict: dict, parasite_dict: dict, tip_mapping: dict, show_interna
     fig = plot_tools.FigureWrapper("Host | Parasite", axes=ax)
     host_tree = utils.dict_to_tree(host_dict, tree.TreeType.HOST)
     parasite_tree = utils.dict_to_tree(parasite_dict, tree.TreeType.PARASITE)
-    _render_helper_host(fig, host_tree.root_node, show_internal_labels)
-    _render_helper_parasite(fig, parasite_tree.root_node, show_internal_labels)
+    _render_helper_host(
+        fig=fig, 
+        node=host_tree.root_node, 
+        show_internal_labels=show_internal_labels, 
+        node_font_size=node_font_size
+    )
+    _render_helper_parasite(
+        fig=fig, 
+        node=parasite_tree.root_node, 
+        show_internal_labels=show_internal_labels,
+        node_font_size=node_font_size
+    )
 
     host_dict = {}
     for host in host_tree.leaf_list():
@@ -53,7 +68,7 @@ def render(host_dict: dict, parasite_dict: dict, tip_mapping: dict, show_interna
                  col=render_settings.GRAY, linestyle='--')
     return fig
 
-def _render_helper_host(fig, node, show_internal_labels):
+def _render_helper_host(fig, node, show_internal_labels, node_font_size):
     """
     Render helper for host tree
     """
@@ -70,12 +85,12 @@ def _render_helper_host(fig, node, show_internal_labels):
 
         # plot node using leaf_layout
         plot_loc = (leaf_layout.col, leaf_layout.row)
-        textbox = fig.text(plot_loc, node.name, size=FONTSIZE, col=render_settings.BLUE, h_a='right')
+        fig.text(plot_loc, node.name, size=node_font_size, col=render_settings.BLUE, h_a='right')
 
     else:
         # recursively call helper function on child nodes
-        _render_helper_host(fig, node.left_node, show_internal_labels)
-        _render_helper_host(fig, node.right_node, show_internal_labels)
+        _render_helper_host(fig, node.left_node, show_internal_labels, node_font_size)
+        _render_helper_host(fig, node.right_node, show_internal_labels, node_font_size)
 
         # get layouts for child nodes to determine position of current node
         right_layout = node.right_node.layout
@@ -93,7 +108,7 @@ def _render_helper_host(fig, node, show_internal_labels):
         # plot node using node_layout
         current_loc = (node.layout.col, node.layout.row)
         if show_internal_labels:
-            fig.text(current_loc, node.name, size=FONTSIZE, col=render_settings.BLUE, h_a='left')
+            fig.text(current_loc, node.name, size=node_font_size, col=render_settings.BLUE, h_a='left')
 
         # draw line from current node to left node
         left_loc = (left_layout.col, left_layout.row)
@@ -106,7 +121,7 @@ def _render_helper_host(fig, node, show_internal_labels):
         fig.line((node.layout.col, right_layout.row), right_loc, col=render_settings.BLACK)
 
 
-def _render_helper_parasite(fig, node, show_internal_labels):
+def _render_helper_parasite(fig, node, show_internal_labels, node_font_size):
     """
     Render helper for parasite tree
     """
@@ -122,12 +137,12 @@ def _render_helper_parasite(fig, node, show_internal_labels):
 
         # plot node using leaf_layout
         plot_loc = (leaf_layout.col, leaf_layout.row)
-        fig.text(plot_loc, node.name, size=FONTSIZE, col=render_settings.BLUE, h_a='left')
+        fig.text(plot_loc, node.name, size=node_font_size, col=render_settings.BLUE, h_a='left')
 
     else:
         # recursively call helper funciton on child nodes
-        _render_helper_parasite(fig, node.left_node, show_internal_labels)
-        _render_helper_parasite(fig, node.right_node, show_internal_labels)
+        _render_helper_parasite(fig, node.left_node, show_internal_labels, node_font_size)
+        _render_helper_parasite(fig, node.right_node, show_internal_labels, node_font_size)
 
         # get layouts for child nodes to determine position of current node
         right_layout = node.right_node.layout
@@ -145,7 +160,7 @@ def _render_helper_parasite(fig, node, show_internal_labels):
         # plot node using node_layout
         current_loc = (node.layout.col, node.layout.row)
         if show_internal_labels:
-            fig.text(current_loc, node.name, size=FONTSIZE, col=render_settings.BLUE, h_a='right')
+            fig.text(current_loc, node.name, size=node_font_size, col=render_settings.BLUE, h_a='right')
 
         # draw line from current node to left node
         left_loc = (left_layout.col, left_layout.row)

commit 7e0e4e994df66bec318e7c2a43d29e4676fb3015

Add show legend checkbox in draw reconciliation (#241)
Add "Display legend" checkbox in reconciliation window. Default is false (no legend).

Resolves #238.

Fixed a bug introduced in #240 where the Display internal node names and Display event frequencies buttons on reconciliation windows do not work.

empress/__init__.py
@@ -96,6 +96,7 @@ class ReconciliationWrapper(Drawable):
                 axes: plt.Axes,
                 show_internal_labels: bool = False,
                 show_freq: bool = True,
+                show_legend: bool = False,
                 node_font_size: float = 0.3,
                 ):
         recon_viewer.render(
@@ -105,6 +106,7 @@ class ReconciliationWrapper(Drawable):
             self.event_frequencies,
             show_internal_labels=show_internal_labels,
             show_freq=show_freq,
+            show_legend=show_legend,
             node_font_size=node_font_size,
             axes=axes
         )

empress/recon_vis/recon_viewer.py
@@ -18,7 +18,7 @@ def render(host_dict: dict,
            event_frequencies: Dict[tuple, float] = None,
            show_internal_labels: bool = False,
            show_freq: bool = False,
-           show_legend: bool = True,
+           show_legend: bool = False,
            node_font_size: float = 0.3,
            axes: Union[plt.Axes, None] = None,
            ):

empress_gui.py
@@ -860,7 +860,7 @@ class TanglegramWindow(tk.Frame):
     
     def draw_tanglegram(self):
         self.fig = App.recon_input.draw(
-            node_font_size=self.font_size
+            node_font_size=self.font_size.get()
         )
         self.canvas = FigureCanvasTkAgg(self.fig, self.tanglegram_frame)
         self.canvas.draw()
@@ -935,6 +935,9 @@ class ReconciliationFrame(tk.Frame):
         self.show_event_frequencies = tk.BooleanVar(value=True)
         self.create_show_event_frequencies_checkbox()
 
+        self.show_legend = tk.BooleanVar(value=False)
+        self.create_show_legend_checkbox()
+
         self.font_size_var = tk.DoubleVar(value=0.3)
         self.font_size = 0.3
         self.create_font_size_editor()
@@ -956,6 +959,14 @@ class ReconciliationFrame(tk.Frame):
             variable=self.show_event_frequencies,
             command=self.update_reconciliation)
         show_event_frequencies_checkbutton.pack(side=tk.LEFT)
+
+    def create_show_legend_checkbox(self):
+        show_legend_checkbutton = tk.Checkbutton(
+            master=self.config_frame,
+            text="Display legend",
+            variable=self.show_legend,
+            command=self.update_reconciliation)
+        show_legend_checkbutton.pack(side=tk.LEFT)
 
     def create_font_size_editor(self):
         font_size_label = tk.Label(
@@ -995,8 +1006,9 @@ class ReconciliationFrame(tk.Frame):
 
     def draw_reconciliation(self):
         self.reconciliation_fig = self.reconciliation.draw(
-            show_internal_labels=self.show_internal_node_names,
-            show_freq=self.show_event_frequencies,
+            show_internal_labels=self.show_internal_node_names.get(),
+            show_freq=self.show_event_frequencies.get(),
+            show_legend=self.show_legend.get(),
             node_font_size=self.font_size
         )
         self.canvas = FigureCanvasTkAgg(self.reconciliation_fig, self.reconciliation_frame)